45e09d20b9e098ef64a057e324d323cf8bacbe1d
daktari-art <denobike@gmail.com>
2026-04-24 10:07:27 +0300

    Clean slate - remove all files

--- components/__init__.py
+++
@@ -1,6 +0,0 @@
-"""UI components for DenLab."""
-from components.chat_interface import ChatInterface
-from components.sidebar import Sidebar
-from components.agent_interface import AgentInterface
-
-__all__ = ["ChatInterface", "Sidebar", "AgentInterface"]
--- features/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Feature modules for DenLab."""
-from features.image_gen import ImageGenerator, generate_image_url
-from features.audio_gen import AudioGenerator
-from features.vision import VisionAnalyzer
-
-__all__ = ["ImageGenerator", "generate_image_url", "AudioGenerator", "VisionAnalyzer"]
