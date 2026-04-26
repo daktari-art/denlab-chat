3e39900c380c4d2dee1b7299d72fab9d6d36a29c
daktari-art <denobike@gmail.com>
2026-04-26 18:06:26 +0300

    Complete replace with denlab1.zip

--- config/__init__.py
+++
@@ -1,9 +0,0 @@
-"""DenLab Chat Config"""
-from .settings import AppConfig, Models, SystemPrompts, DeveloperConfig
-from .models import UserSession, DeveloperSession, ConversationConfig, Session, ChatMessage, MessageRole
-
-__all__ = [
-    "AppConfig", "Models", "SystemPrompts", "DeveloperConfig",
-    "UserSession", "DeveloperSession", "ConversationConfig",
-    "Session", "ChatMessage", "MessageRole"
-]
--- core/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Core modules for DenLab."""
-from core.api_client import PollinationsClient, get_client
-from core.session_manager import SessionManager
-
-__all__ = ["PollinationsClient", "get_client", "SessionManager"]
